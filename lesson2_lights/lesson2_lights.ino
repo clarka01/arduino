a2f7aad30c17a64e4bf700ab7e4e918dc2d209df
Aaron Clark <78091625+clarka01@users.noreply.github.com>
2025-12-26 22:49:00 -0500

    committing for vscode ide

--- lesson2_lights/lesson2_lights.ino
+++ lesson2_lights/lesson2_lights.ino
@@ -69,5 +69,5 @@ void loop() {
   digitalWrite(CABIN_LIGHTS_PIN, HIGH);  // This line turns the lander's light ON.
   delay(100);                           // Wait for one second (1000 milliseconds) with the light ON.
   digitalWrite(CABIN_LIGHTS_PIN, LOW);   // This line turns the lander's light OFF.
-  delay(1000);                            // Wait for a tenth of a second (100 milliseconds) with the light OFF.
+  delay(10000);                            // Wait for a tenth of a second (100 milliseconds) with the light OFF.
 }
